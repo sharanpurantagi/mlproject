fee25baa68a16594f0b282010d01514021cdefb9
sharanpurantagi <sharanpurantagi@gmail.com>
2026-02-24 16:05:30 +0530

    yoooooo

--- src/utils.py
+++ src/utils.py
@@ -39,6 +39,18 @@ def evaluate_models(x_train, x_test, y_train, y_test, models ,  params):
             report[model_name] = test_model_score
 
         return report
-
+  
     except Exception as e:
         raise CustomException(e, sys)
+    
+      
+def load_object(file_path):
+    try:
+        with open(file_path , "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+
+ 
